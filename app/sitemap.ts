049ce1aa714f5048b72d2b790833526d462555f2
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-05-11 12:50:59 +0530

    daily crawl

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.rahuljavascript.xyz",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "daily",
       priority: 1,
     },
   ];
